e1c57e807e3c09023734614b6c9980dfa967dd9f
SaraBenti <sara.bentivegni@enaipriminifad.org>
2023-05-03 12:56:02 +0200

    ex

--- calc-app/src/components/Display.js
+++ calc-app/src/components/Display.js
@@ -2,7 +2,20 @@ import React from "react";
 
 class Display extends React.Component {
     render() {
-        return (<div className="col-12 text-bg-dark p-3 text-end">{this.props.risultato}</div>);
+        let risultatoNumero = parseFloat(this.props.risultato);
+        let numeroFormat = new Intl.NumberFormat('it-IT', { maximumSignificantDigits: 6 }).format(risultatoNumero);
+        let fsCss = "fs-1 min-height-150px";
+        if (risultatoNumero >= 10000000) {
+            fsCss = "fs-5 min-height-80px";
+        }
+        return (<div className="col-12 text-bg-dark p-3 text-end">
+            <div className="fs-5">
+                {this.props.operatore === null ? "0" : this.props.operatore} {this.props.azione}
+            </div>
+            <div className={fsCss}>
+                {numeroFormat}
+            </div>
+        </div>);
     }
 }
 
--- calc-app/src/components/PulsanteNumero.js
+++ calc-app/src/components/PulsanteNumero.js
@@ -1,11 +1,8 @@
 import React from "react";
 
 class PulsanteNumero extends React.Component {
-    onClickNumero() {
-        this.setState("displayNumero", this.props.label);
-    }
     render() {
-        return (<button onClick={this.onClickNumero} className="w-100 btn btn-light">{this.props.label}</button>);
+        return (<button onClick={this.props.onClick} className="w-100 btn btn-light">{this.props.label}</button>);
     }
 }
 
